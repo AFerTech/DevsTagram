e736b6d6fcb2bb7fad309a9a5008241d01bb5df8
Alexis Fernando <alexis96fer@gmail.com>
2022-10-08 12:57:54 -0500

    iniciando con estilos y agregando un header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,19 +3,37 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> --}}
         @vite('resources/css/app.css')
         <title>DevsTagram - @yield('titulo')</title>
+        <script src=" {{ asset('js/app.js') }}" defer></script>
     </head>
-    <body>
-        <nav>
-            <a href="/">Principal</a>
-            <a href="/nosotros">Nosotros</a>
-            <a href="tienda">Tienda</a>
-        </nav>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
 
-        <h1>@yield('titulo')</h1>
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevsTagram</h1>
 
-        <h2>@yield('contenido')</h2>
+            <nav class="flex gap-2 items-center">
+                <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
+                <a href="#" class="font-bold uppercase text-gray-600 text-sm">Crear Cuenta</a>
+            </nav>
+
+            </div>
+
+        </header>  {{--Fin del Header--}}
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+        </main> {{--Fin del main--}}
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            DevsTagram - Todos los Derechos Reservados 
+            {{now ()->year}}
+        </footer> {{--Fin del footer--}}
+
     </body>
     
 </html>   
