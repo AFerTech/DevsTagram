c51cb6cbb15555f91664079ff2d6086f06277238
Alexis Fernando <alexis96fer@gmail.com>
2022-10-14 14:53:00 -0500

    estilos al formulario de publicaciones

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -14,25 +14,48 @@
             <form action="{{route('register')}}" method="POST" >
                 @csrf
                 <div class="mb-5x">
-                    <label for="name"  class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                    <label for="titulo"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Título
                     </label>
                     <input 
                     type="text"
-                    id="name"
-                    name="name"
-                    placeholder="Nombre"
-                    class="border p-3 w-full rounded-lg @error('name')
+                    id="titulo"
+                    name="titulo"
+                    placeholder="Título"
+                    class="border p-3 w-full rounded-lg @error('titulo')
                     border-red-600
                     @enderror"
-                    value="{{old ('name')}}"
+                    value="{{old ('titulo')}}"
                     />                    
-                    @error('name')
+                    @error('titulo')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                     {{$message}}
                     </p>
                     @enderror
-                </div>
+                </div> {{--titulo--}}
+                <div class="mb-5x">
+                    <label for="descricion"  class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea 
+                    id="descricion"
+                    name="descricion"
+                    placeholder="Descripción"
+                    class="border p-3 w-full rounded-lg @error('descripcion')
+                    border-red-600
+                    @enderror"
+                    >{{old ('descricion')}}</textarea>                    
+                    @error('descricion')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                    {{$message}}
+                    </p>
+                    @enderror
+                </div>  {{--descripcion--}}
+                <input 
+                type="submit" 
+                value="Publicar" 
+                class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
+                uppercase font-bold w-full p-3 text-white rounded-lg mt-10">
             </form>
         </div>
 
