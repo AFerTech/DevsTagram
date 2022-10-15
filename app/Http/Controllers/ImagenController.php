d4db411abe1523c96fff6a2ead401c9815e8eeb1
Alexis Fernando <alexis96fer@gmail.com>
2022-10-15 12:18:36 -0500

    rutas y controlador para la subida de img

---
+++ app/Http/Controllers/ImagenController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImagenController extends Controller
+{
+    public function store(Request $request)
+    {
+        $img = $request->file('file');
+        return response()->json(['img' => $img->extension()]);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> --}}
+        @stack('styles')
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
         <title>DevsTagram - @yield('titulo')</title>
