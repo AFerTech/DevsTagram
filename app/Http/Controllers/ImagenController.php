22d286ebd4477f99d3ef9048a8ae4fadb9ad7305
Alexis Fernando <alexis96fer@gmail.com>
2022-10-15 13:23:54 -0500

    funcionalidad para almacenar img en el servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
         $img = $request->file('file');
-        return response()->json(['img' => $img->extension()]);
+
+        $nombreImg = Str::uuid() . "." . $img->extension();
+
+        $imgServidor =Image::make($img);
+        $imgServidor->fit(1000,1000, null, 'center');
+
+        $imgPath = public_path('uploads') . '/' . $nombreImg;
+
+        $imgServidor->save($imgPath);
+
+        return response()->json(['img' => $nombreImg]);
     }
 }
