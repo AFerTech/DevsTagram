3fa2699a1e234c0fb0e65fd233e74846c533e51a
Alexis Fernando <alexis96fer@gmail.com>
2022-10-28 00:03:26 -0500

    enlace para editar perfil

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegistrerController;
 use Illuminate\Support\Facades\Route;
@@ -44,5 +45,9 @@ Route::post('/imagenes',[ImagenController::class, 'store'])->name('imagenes.stor
 
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes,store');
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes,destroy');
+
+Route::get('{user:username}/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('{user:username}/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
+
 
 
